d7fb2d028146c4de6f36dd29716c3ed20f5d6fd8
tusarimrananik <tusarimrananik@gmail.com>
2024-09-01 14:20:13 +0600

    working but not in loop

--- index.js
+++ index.js
@@ -9,35 +9,25 @@ const puppeteer = require('puppeteer');
         args: ['--start-maximized']
     }); // Set to true for headless mode
 
-
     var [page] = await browser.pages();
-
     // Navigate to the page with the form
-    await page.goto('http://www.educationboardresults.gov.bd/'); // Replace with your form page URL
+    await page.setDefaultNavigationTimeout(60000); // Sets a 60-second timeout globally
 
+    await page.goto('http://www.educationboardresults.gov.bd/'); // Replace with your form page URL
 
     // Wait for the body element to be present
     await page.waitForSelector('body');
-
     // Simulate a click at coordinates (0, 0) or any other point in the page
-
 
     // Select the Name of Examination
     await page.select('#exam', 'hsc'); // Use the value of the option you want to select
-
     // Select the Year of Examination
     await page.select('#year', '2023'); // Use the value of the option you want to select
-
     // Select the Name of Board
     await page.select('#board', 'rajshahi'); // Use the value of the option you want to select
 
-
     await page.type('#roll', '106369'); // Replace with actual roll number
     await page.type('#reg', '1812628169'); // R
-
-
-
-
 
     const result = await page.evaluate(() => {
         const element = document.querySelector('body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > form > table > tbody > tr > td:nth-child(2) > fieldset > table > tbody > tr:nth-child(7) > td:nth-child(2)');
@@ -49,49 +39,46 @@ const puppeteer = require('puppeteer');
 
     await page.click('#button2');
 
-    await page.waitForNavigation();
+
+    await page.waitForSelector('.black12 tr:nth-child(1) td:nth-child(4)');
+
+    await page.waitForSelector('.black12 tr:nth-child(2) td:nth-child(4)');
+
+    await page.waitForSelector('.black12 tr:nth-child(3) td:nth-child(4)');
+
+    await page.waitForSelector('.black12 tr:nth-child(5) td:nth-child(4)');
+
+    await page.waitForSelector('.black12 tr:nth-child(6) td:nth-child(2)');
 
 
 
 
     const information = await page.evaluate(() => {
+
         function extractInfo() {
             const info = {};
-            // Function to find the next sibling <td> with text content
-            function getNextTdTextContent(td) {
-                let nextTd = td.nextElementSibling;
-                while (nextTd && nextTd.tagName !== 'TD') {
-                    nextTd = nextTd.nextElementSibling;
-                }
-                return nextTd ? nextTd.innerText.trim() : '';
-            }
-            // Helper function to find <td> by text content
-            function findTdByText(text) {
-                const tds = document.querySelectorAll('td');
-                for (let td of tds) {
-                    if (td.innerText.trim() === text) {
-                        return td;
-                    }
-                }
-                return null;
-            }
-            // Extract information based on known text label
-            info.result = getNextTdTextContent(findTdByText('Result'));
-            info.institute = getNextTdTextContent(findTdByText('Institute'));
-            info.gpa = getNextTdTextContent(findTdByText('GPA'));
+
+
+
+            info.name = document.querySelector(".black12 tr:nth-child(1) td:nth-child(4)").innerText;
+
+            info.father = document.querySelector(".black12 tr:nth-child(2) td:nth-child(4)").innerText;
+
+            info.mother = document.querySelector(".black12 tr:nth-child(3) td:nth-child(4)").innerText;
+
+            info.institute = document.querySelector(".black12 tr:nth-child(5) td:nth-child(4)").innerText;
+            info.gpa = document.querySelector(".black12 tr:nth-child(6) td:nth-child(2)").innerText;
+
+
+
+
+
             return info;
         }
 
-        // Example usage
         return extractInfo();
-    });
-
-
-    console.log(information)
-
-
-
-
+    })
+    console.log(information);
 
 
 
