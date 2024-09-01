5f95beb47ae1396938b251def27addf92b3bb6b4
tusarimrananik <tusarimrananik@gmail.com>
2024-09-01 15:45:39 +0600

    working with loop

--- index.js
+++ index.js
@@ -2,85 +2,84 @@ const puppeteer = require('puppeteer');
 
 
 (async () => {
-    // Launch the browser
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
         args: ['--start-maximized']
-    }); // Set to true for headless mode
-
-    var [page] = await browser.pages();
-    // Navigate to the page with the form
-    await page.setDefaultNavigationTimeout(60000); // Sets a 60-second timeout globally
-
-    await page.goto('http://www.educationboardresults.gov.bd/'); // Replace with your form page URL
-
-    // Wait for the body element to be present
-    await page.waitForSelector('body');
-    // Simulate a click at coordinates (0, 0) or any other point in the page
-
-    // Select the Name of Examination
-    await page.select('#exam', 'hsc'); // Use the value of the option you want to select
-    // Select the Year of Examination
-    await page.select('#year', '2023'); // Use the value of the option you want to select
-    // Select the Name of Board
-    await page.select('#board', 'rajshahi'); // Use the value of the option you want to select
-
-    await page.type('#roll', '106369'); // Replace with actual roll number
-    await page.type('#reg', '1812628169'); // R
-
-    const result = await page.evaluate(() => {
-        const element = document.querySelector('body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > form > table > tbody > tr > td:nth-child(2) > fieldset > table > tbody > tr:nth-child(7) > td:nth-child(2)');
-        const expression = element.innerText; // Get the inner text
-        return eval(expression); // Evaluate the mathematical expression
     });
 
-    await page.type('#value_s', result.toString()); // R
-
-    await page.click('#button2');
 
 
-    await page.waitForSelector('.black12 tr:nth-child(1) td:nth-child(4)');
-
-    await page.waitForSelector('.black12 tr:nth-child(2) td:nth-child(4)');
-
-    await page.waitForSelector('.black12 tr:nth-child(3) td:nth-child(4)');
-
-    await page.waitForSelector('.black12 tr:nth-child(5) td:nth-child(4)');
-
-    await page.waitForSelector('.black12 tr:nth-child(6) td:nth-child(2)');
+    function wait(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
 
 
+    var [page] = await browser.pages();
+    await page.setDefaultNavigationTimeout(60000);
+    let roll = 109343 - 2;
+    let reg = 1812628169 - 2;
 
-    const information = await page.evaluate(() => {
-
-        function extractInfo() {
-            const info = {};
-
-
-
-            info.name = document.querySelector(".black12 tr:nth-child(1) td:nth-child(4)").innerText;
-
-            info.father = document.querySelector(".black12 tr:nth-child(2) td:nth-child(4)").innerText;
-
-            info.mother = document.querySelector(".black12 tr:nth-child(3) td:nth-child(4)").innerText;
-
-            info.institute = document.querySelector(".black12 tr:nth-child(5) td:nth-child(4)").innerText;
-            info.gpa = document.querySelector(".black12 tr:nth-child(6) td:nth-child(2)").innerText;
+    await page.goto('http://www.educationboardresults.gov.bd/');
 
 
+    for (let i = 0; i < 500; i++) {
+        await page.waitForSelector('body');
+        await page.select('#exam', 'ssc_voc');
+        await page.select('#year', '2021'); //
+        await page.select('#board', 'rajshahi');
+        await page.type('#roll', `${(roll).toString()}`);
+        // await wait(3000);
+        await page.type('#reg', `${(reg).toString()}`); // R
+        // await wait(3000);
+        const result = await page.evaluate(() => {
+            const element = document.querySelector('body > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > form > table > tbody > tr > td:nth-child(2) > fieldset > table > tbody > tr:nth-child(7) > td:nth-child(2)');
+            const expression = element.innerText;
+            return eval(expression);
+        });
+        await page.type('#value_s', result.toString());
+        await page.click('#button2');
 
 
 
-            return info;
+        let findName;
+        try {
+            findName = await page.waitForSelector('.black12 tr:nth-child(1) td:nth-child(4)', { timeout: 500 });
+        } catch (error) {
+            findName = false;
+        }
+        if (findName) {
+            await page.waitForSelector('.black12 tr:nth-child(1) td:nth-child(4)');
+            await page.waitForSelector('.black12 tr:nth-child(2) td:nth-child(4)');
+            await page.waitForSelector('.black12 tr:nth-child(3) td:nth-child(4)');
+            await page.waitForSelector('.black12 tr:nth-child(5) td:nth-child(4)');
+            await page.waitForSelector('.black12 tr:nth-child(6) td:nth-child(2)');
+            const information = await page.evaluate(() => {
+                function extractInfo() {
+                    const info = {};
+                    info.name = document.querySelector(".black12 tr:nth-child(1) td:nth-child(4)").innerText;
+                    info.father = document.querySelector(".black12 tr:nth-child(2) td:nth-child(4)").innerText;
+                    info.mother = document.querySelector(".black12 tr:nth-child(3) td:nth-child(4)").innerText;
+                    info.institute = document.querySelector(".black12 tr:nth-child(5) td:nth-child(4)").innerText;
+                    info.gpa = document.querySelector(".black12 tr:nth-child(6) td:nth-child(2)").innerText;
+                    return info;
+                }
+                return extractInfo();
+            })
+            information.roll = roll
+            information.reg = reg
+            console.log(information);
+            await page.reload();
+            roll++;
+            reg++;
+        } else {
+            reg++;
+            // await page.goto('http://www.educationboardresults.gov.bd/');
         }
 
-        return extractInfo();
-    })
-    console.log(information);
 
-
+    }
 
 
 })();
